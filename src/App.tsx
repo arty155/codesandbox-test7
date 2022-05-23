236054a3b4ae826b0ac2e70b51b710f0212f09a8
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-23 20:30:22 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
 import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
 import "./styles.css";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -24,6 +18,7 @@ export default function App() {
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
